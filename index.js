c0ec8d9f86c6fd6814e7c53b84c1fcf5f3f2513b
BearJ <bearverygood@126.com>
2015-09-28 17:42:07 +0800

    ver 1.0.4

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var _sockets = [];
 function handler(opts){
 
     var MINE = { '.bmp': 'image/bmp', '.css': 'text/css', '.doc': 'application/msword', '.dtd': 'text/xml', '.gif': 'image/gif', '.hta': 'application/hta', '.htc': 'text/x-component', '.htm': 'text/html', '.html': 'text/html', '.tmpl': 'text/html', '.xhtml' : 'text/html', '.ico': 'image/x-icon', '.jpe': 'image/jpeg', '.jpeg': 'image/jpeg', '.jpg': 'image/jpeg', '.js': 'text/javascript', '.json': 'application/json', '.mocha' : 'text/javascript', '.mp3': 'audio/mp3', '.mp4': 'video/mpeg4', '.mpeg': 'video/mpg', '.mpg': 'video/mpg', '.manifest' : 'text/cache-manifest', '.pdf': 'application/pdf', '.png': 'image/png', '.ppt': 'application/vnd.ms-powerpoint', '.rmvb': 'application/vnd.rn-realmedia-vbr', '.rm': 'application/vnd.rn-realmedia', '.rtf': 'application/msword', '.svg': 'image/svg+xml', '.swf': 'application/x-shockwave-flash', '.tif': 'image/tiff', '.tiff': 'image/tiff', '.txt': 'text/plain', '.vml': 'text/xml', '.vxml': 'text/xml', '.wav': 'audio/wav', '.wma': 'audio/x-ms-wma', '.wmv': 'video/x-ms-wmv', '.woff': 'image/woff', '.xml': 'text/xml', '.xls': 'application/vnd.ms-excel', '.xq': 'text/xml', '.xql': 'text/xml', '.xquery' : 'text/xml', '.xsd': 'text/xml', '.xsl': 'text/xml', '.xslt': 'text/xml'};
-    var socketjs = fs.readFileSync(__dirname + '/lib/socket.io-1.3.4.js', 'utf-8');
+    var socketjs = fs.readFileSync(__dirname + '/lib/socket.io-1.3.4.js', 'utf8');
 
     return function(req, res){
         if (req.method !== 'GET' && req.method !== 'HEAD') {
@@ -54,7 +54,7 @@ function handler(opts){
         //如果是目录就列出来
         if (fs.lstatSync(abs).isDirectory()){
             var files = fs.readdirSync(abs);
-            var result = fs.readFileSync(__dirname + '/lib/tree.html', 'utf-8');
+            var result = fs.readFileSync(__dirname + '/lib/tree.html', 'utf8');
             var render = '';
             files.forEach(function (file) {
                 if(file.charAt(0) === '.'){
@@ -72,7 +72,7 @@ function handler(opts){
 
         var extname = path.extname(abs);
         var mine = MINE[extname] ? MINE[extname] : 'text/plain';
-        if(mine.indexOf('text') !== -1) mine += '; charset=utf-8';
+        if(mine.indexOf('text') !== -1) mine += '; charset=utf8';
 
         res.writeHead(200, {
             'Content-Type': mine,
@@ -89,7 +89,7 @@ function handler(opts){
         var content = fs.readFileSync(abs), encoding = opts.encoding, pageEncoding;
         pageEncoding = content.toString().match(/<%##encode:\s*(.*?)##%>/);
         if(pageEncoding && pageEncoding.length > 1) encoding = pageEncoding[1];
-        if(!iconv.encodingExists(encoding)) encoding = 'utf-8';
+        if(!iconv.encodingExists(encoding)) encoding = 'utf8';
         content = iconv.decode(content, encoding).toString();
 
         // 如果开启了模板编译
@@ -97,7 +97,7 @@ function handler(opts){
             var context = {};
             if(fs.existsSync(opts.tmpl)){
                 try{
-                    context = eval('(' + fs.readFileSync(opts.tmpl, 'utf-8') + ')');
+                    context = eval('(' + fs.readFileSync(opts.tmpl, 'utf8') + ')');
                 }catch(err){
                     throw new Error('parse context file error, please check context file ' + opts.tmpl);
                 }
@@ -127,13 +127,14 @@ function handler(opts){
  * @param {Object} options default: {port: 8080}
  */
 function start(options){
-    var opts = lodash.extend({port: 8080, root: __dirname, encoding: 'utf-8', live: true, tmpl: false}, options);
+    var opts = lodash.extend({port: 8080, root: __dirname, encoding: 'utf8', live: true, tmpl: false}, options);
     var app = http.createServer(handler(opts));
     var io = socket(app);
 
     app.listen(opts.port, function(){
         var url = 'http://127.0.0.1' + (opts.port == 80 ? '' : ':' + opts.port);
         console.log('Server start on ' + url + '.');
+        require('child_process').exec('start "" "' + url + '"');
     });
 
     // listen with socket
